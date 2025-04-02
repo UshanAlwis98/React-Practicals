e9aa038e2d75ffeb19d23dd2055a51b59b613045
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-03 04:52:26 +0530

    ES-6 aDDED

---
+++ Side Projects/Js-ES6.js
@@ -0,0 +1,72 @@
+const Tvseries = [
+  {
+    id: 1,
+    Title: "Game of Thrones",
+    Company: "Warner Brothers",
+    publicationDate: "2025-01-01",
+    Genres: ["Adventure", "Novel", "Fantasy", "Black Comedy"],
+  },
+
+  {
+    id: 2,
+    Title: "Prison Break",
+    Company: "EA Sports",
+    publicationDate: "2026-02-01",
+    Genres: ["Fantasy", "Novel", "Adventure", "Black Comedy"],
+    Reviews: {
+      goodReads: {
+        rating: 1.5,
+        ratingsCount: 0,
+        reviewsCount: 10,
+      },
+
+      Librarything: {
+        rating: 2.5,
+        ratingsCount: 87,
+        reviewsCount: 11,
+      },
+    },
+  },
+
+  {
+    id: 3,
+    Title: "Halloween",
+    publicationDate: "2027-03-01",
+    Reviews: {
+      goodReads: {
+        rating: 1.5,
+        ratingsCount: 1,
+        reviewsCount: 10,
+      },
+    },
+  },
+];
+
+//ES5 Old Function
+
+// function myOldFunction(x, y) {
+//   return x + y;
+// }
+
+// console.log(
+//   myOldFunction(
+//     Tvseries[1].Reviews.Librarything.ratingsCount,
+//     Tvseries[2].Reviews.goodReads.reviewsCount
+//   )
+// );
+
+//ES6 New Arrow Function
+
+const newArrowFunction = (k, z) => k + z;
+
+console.log(
+  newArrowFunction(
+    Tvseries[1].Reviews.Librarything.ratingsCount,
+    Tvseries[2].Reviews.goodReads.reviewsCount
+  )
+);
+
+const Destrcucturingpart = {
+  Tvseries: [0].title,
+  Tvseries: [1].publicationDate,
+};
