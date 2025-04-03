8902635b4906e39f10296bb4a427883593289adc
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-03 20:32:18 +0530

    Inline CSS Added.

--- Traversy Media Crash Course/my-react-app-crash/src/App.jsx
+++ Traversy Media Crash Course/my-react-app-crash/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const name = "Ushan";
+  const x = 5;
+  const y = 1;
+  const names = ["Ushan", "Loshitha", "Alwis"];
+  const loggedIn = true;
+
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p style={{ color: "purple", fontSize: "24px" }}>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((mynames, index) => (
+          <li key={index}> {mynames}</li>
+        ))}
+      </ul>
+      {loggedIn ? <h1> Hello Member</h1> : <h1>You Have Not Logged in yet</h1>}
+    </>
+  );
 };
 
 export default App;
--- Traversy Media Crash Course/my-react-app-crash/tailwind.config.js
+++ Traversy Media Crash Course/my-react-app-crash/tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       gridTemplateColumns: {
         "70/30": "70%, 28%",
       },
+      fontSize: {
+        "140px": "40px",
+      },
     },
   },
   plugins: [],
