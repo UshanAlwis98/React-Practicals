14325f8c61d10ff1220caddf447ce02767989602
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-04 17:02:42 +0530

    elements added.

--- Self-Learing-Project/my-app/src/Components/NavBar.jsx
+++ Self-Learing-Project/my-app/src/Components/NavBar.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const NavBar = () => {
   return (
     <>
-      <div class="flex">
+      <div class="flex gap-14">
         <div class="w-64 flex-none bg-blue-500 ">Hello World</div>
         <input
-          class="w-64 flex-1e bg-blue-500"
+          class="w-64 flex-1e bg-blue-500 rounded-md"
           type="text"
           border
           placeholder="Search..."
         />
+
+        <button class="rounded-full bg-blue-500 w-64 border-2 border-solid border-neutral-500">
+          Save Changes
+        </button>
       </div>
     </>
   );
