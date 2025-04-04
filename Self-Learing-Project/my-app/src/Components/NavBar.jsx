cb49847b2924ab807c24db21c31a2dccdd50cb64
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-04 16:30:28 +0530

    Flex Added.

--- Self-Learing-Project/my-app/src/Components/NavBar.jsx
+++ Self-Learing-Project/my-app/src/Components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div class="flex">
+        <div class="w-64 flex-none bg-blue-500 ">Hello World</div>
+        <input
+          class="w-64 flex-1e bg-blue-500"
+          type="text"
+          border
+          placeholder="Search..."
+        />
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
