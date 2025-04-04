8578f74cc8f416ce3b9848618900b55661a4d88d
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-04 23:36:46 +0530

    NavBar Added.

--- Self-Learing-Project/my-app/src/App.jsx
+++ Self-Learing-Project/my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
+
 const App = () => {
   return <NavBar />;
 };
--- Self-Learing-Project/my-app/src/Components/NavBar.jsx
+++ Self-Learing-Project/my-app/src/Components/NavBar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 const NavBar = () => {
   return (
     <>
-      <div class="flex gap-14">
-        <div class="w-64 flex-none bg-blue-500 ">Hello World</div>
-        <input
-          class="w-64 flex-1e bg-blue-500 rounded-md"
-          type="text"
-          border
-          placeholder="Search..."
-        />
+      <div className="bg-indigo-500 flex items-center justify-between px-8 py-4">
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="w-12 h-12 rounded-full" />
+        </div>
 
-        <button class="rounded-full bg-blue-500 w-64 border-2 border-solid border-neutral-500">
-          Save Changes
-        </button>
+        <div className="text-center font-mono text-4xl text-slate-950 uppercase font-extrabold  tracking-widest">
+          <h1>Talent Gateway Recruiters</h1>
+        </div>
+
+        <div className="flex justify-end gap-8 items-center font-mono text-2xl ">
+          <a href="#">Home</a>
+          <a href="#">Jobs</a>
+          <a href="#" className="inline-block w-54">
+            Contact Me
+          </a>
+        </div>
       </div>
     </>
   );
