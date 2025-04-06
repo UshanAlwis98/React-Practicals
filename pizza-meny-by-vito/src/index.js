a85ce38432499b288701e862ef6a31b90f96a6cf
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-07 04:20:48 +0530

    Created More Components.

--- pizza-meny-by-vito/src/index.js
+++ pizza-meny-by-vito/src/index.js
@@ -49,10 +49,34 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React !!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Company</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're Currently Open</footer>
+  );
+
+  // React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
