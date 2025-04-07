a75ef6d9379f6f4b9f3c76c171fdbeb12edac31a
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-07 22:13:59 +0530

    Challange Complted.

--- My Challange 01/my-challange-01/src/index.js
+++ My Challange 01/my-challange-01/src/index.js
@@ -5,7 +5,55 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <img src="Assests/Images/jonas.jpeg" />
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src="/Assests/Images/jonas.jpeg"
+      alt="Jonas Profile Photo"
+      className="avatar"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1> Ushan Loshitha</h1>
+
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill emoji="💪" skillName="React" color="purple"></Skill>
+      <Skill emoji="💪" skillName="HTML+CSS" color="red"></Skill>
+      <Skill emoji="💪" skillName="JavaScript" color="yellow"></Skill>
+      <Skill emoji="👌" skillName="Svelete" color="green"></Skill>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <div>{props.skillName}</div>
+      <div>{props.emoji}</div>
     </div>
   );
 }
