ddea157967984bcde411bf2048cfcb54efa21cad
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-08 02:21:29 +0530

    First Steps Achived.

--- Self-Learing-Project/Just React/my-portfolio-app/src/App.jsx
+++ Self-Learing-Project/Just React/my-portfolio-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
+import HeroSection from "./Components/HeroSection";
 import "./index.css";
 const App = () => {
   return (
     <div>
       <NavBar />
+      <HeroSection />
     </div>
   );
 };
---
+++ Self-Learing-Project/Just React/my-portfolio-app/src/Components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="myHeroSectionImage">
+      <div>
+        <h1 className="heroSectionFirstHeading">Ushan Alwis</h1>
+        <h1 className="heroSectionSecondHeading"> Portfolio </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
